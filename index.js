4a5f6c5aa1c1dddc21dcec2846e2d7ba68a3b4f8
Erika <56351236+ErikaDelaRosa@users.noreply.github.com>
2020-04-29 12:20:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-function changeColor () {
-    event.preventDefault();// when you a  button inside of a form this is a must. 
-    form.style.backgroundColor ="pink";
+
+/*
+function sumValue () {
+   // var num1, num2, result;
+    num1 = Number(document.formCalculator.num1.value);
+    num2 = Number(document.formCalculator.num2.value);
+    res = num1 + num2 ; 
+    document.formCalculator.result.value = res;
+};
+*/
+function add () {
+    var a = document.getElementById("one").value;
+    var b = document.getElementsById("two").value;
+    var c = a + b;
+    document.getElementById("answer").value=+c;
 }
+function sub () {
+    var a = document.getElementById("one").value;
+    var b = document.getElementsById("two").value;
+    var c= a - b;
+    document.getElementById("answer").value=+c;
+}
+function mul () {
+    var a = document.getElementById("one").value;
+    var b = document.getElementsById("two").value;
+    var c= a * b;
+    document.getElementById("answer").value=+c;
+}
+function div () {
+    var a = document.getElementById("one").value;
+    var b = document.getElementsById("two").value;
+    var c= a % b;
+    document.getElementById("answer").value=+c;
+}
+
+
